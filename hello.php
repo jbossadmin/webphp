0f06594e8bbb108f8e947ad50c7356571e887844
Praveen <praveen.mali@imaginea.com>
2017-07-14 16:08:28 +0530

    First File

---
+++ hello.php
@@ -0,0 +1,17 @@
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to DevOps!</h1>
+<p>Hello World.</p>
+
+</body>
+</html>
+
